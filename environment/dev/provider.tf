06c24f7e3159c3e965e47e634c0b2467743a6bde
unknown <sumitgupta21051@gmail.com>
2025-09-03 02:58:56 +0530

    sa2

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,16 +1,18 @@
 terraform {
+    backend "azurerm" {
+        resource_group_name = "sunil_rg"
+        storage_account_name = "sunilstg2025"
+        container_name = "container2025"
+        key = "rg.terraform.tfstate"
+    }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.42.0"
     }
   }
-    # backend "azurerm" {
-    #     resource_group_name = "sunil_rg"
-    #     storage_account_name = "sunilstg2025"
-    #     container_name = "container2025"
-    #     key = "rg.terraform.tfstate"
-    # }
+
 }
 
 provider "azurerm" {
