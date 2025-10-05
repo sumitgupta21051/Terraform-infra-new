f6c405ca7ff18b06fc7bea86d8ba3d5e783d75f5
sumitgupta21051 <sumitgupta21051@gmail.com>
2025-10-06 02:59:47 +0530

    Update provider.tf

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -7,13 +7,13 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "sumit"       # Can also be set via `ARM_TENANT_ID` environment variable. Azure CLI will fallback to use the connected tenant ID if not supplied.
-    storage_account_name = "sumit123456" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "sumit123456" # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    storage_account_name = "sumit7838" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "sumit7838" # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "76095b6e-0585-402e-8e43-281835aebfd1"
+  subscription_id = "b4235c62-725e-4da6-9d12-296ccb04cb57"
 }
